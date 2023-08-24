966d38963971a8b42fad89190fd71b9f9b86f819
opifex <opifexM@outlook.com>
2023-08-24 16:25:13 -0400

    4.17. Update data model's date fields to use Date type

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -10,35 +10,22 @@ class SortView extends View {
   createHtml() {
     return html`
         <!-- <form class="trip-events__trip-sort  trip-sort" action="#" method="get"> -->
-        <div class="trip-sort__item  trip-sort__item--day">
-            <input checked="" class="trip-sort__input  visually-hidden" id="sort-day" name="trip-sort"
-                   type="radio" value="sort-day">
-            <label class="trip-sort__btn" for="sort-day">Day</label>
-        </div>
-
-        <div class="trip-sort__item  trip-sort__item--event">
-            <input class="trip-sort__input  visually-hidden" disabled="" id="sort-event" name="trip-sort"
-                   type="radio" value="sort-event">
-            <label class="trip-sort__btn" for="sort-event">Event</label>
-        </div>
-
-        <div class="trip-sort__item  trip-sort__item--time">
-            <input class="trip-sort__input  visually-hidden" id="sort-time" name="trip-sort" type="radio"
-                   value="sort-time">
-            <label class="trip-sort__btn" for="sort-time">Time</label>
-        </div>
-
-        <div class="trip-sort__item  trip-sort__item--price">
-            <input class="trip-sort__input  visually-hidden" id="sort-price" name="trip-sort" type="radio"
-                   value="sort-price">
-            <label class="trip-sort__btn" for="sort-price">Price</label>
-        </div>
-
-        <div class="trip-sort__item  trip-sort__item--offer">
-            <input class="trip-sort__input  visually-hidden" disabled="" id="sort-offer" name="trip-sort"
-                   type="radio" value="sort-offer">
-            <label class="trip-sort__btn" for="sort-offer">Offers</label>
-        </div>
+        ${this.state.items.map((item) => html`
+          <div class="trip-sort__item  trip-sort__item--day">
+              <input class="trip-sort__input  visually-hidden"
+                     id="sort-${item.value}"
+                     name="trip-sort"
+                     type="radio"
+                     value="sort-${item.value}"
+                     ${item.isSelected ? 'checked' : ''}
+                     ${item.isDisabled ? 'disabled' : ''}
+              >
+              <label class="trip-sort__btn"
+                     for="sort-${item.value}"
+              >${item.value}
+              </label>
+          </div>
+        `)}
     `;
   }
 }
